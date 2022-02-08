12e722a146038a56493e97aa38791adb316286a9
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-02-08 23:41:14 +0530

    InitialCommit

---
+++ Day 9/javascript.js
@@ -0,0 +1,46 @@
+var a;
+
+a = 10
+console.log(a)
+console.log(typeof a)
+
+a = "hello"
+console.log(a)
+console.log(typeof a)
+
+a = true;
+console.log(a)
+console.log(typeof a)
+
+a = null;
+console.log(a)
+console.log(typeof a)
+
+a = 2.4
+console.log(a)
+console.log(typeof a)
+
+a = 'How are u'
+console.log(a)
+console.log(typeof a)
+
+a = `How yoooo`
+console.log(a)
+console.log(typeof a)
+
+a = "How are u" +
+    "i am fine"
+console.log(a)
+console.log(typeof a)
+
+a = `How are u
+i am fine`;
+console.log(a)
+console.log(typeof a)
+
+var num = 231
+console.log(`half of ${num} is ${num/2}`)
+
+//allowed redeclaration
+var b = 10
+var c = 10
