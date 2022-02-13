83c1106758f0a99bf12456ebfa7f74acddcd8384
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-02-13 22:59:06 +0530

    InitialCommit

--- Day 11/strings.js
+++ Day 11/strings.js
@@ -5,6 +5,7 @@ console.log(str[4])
 console.log(str[6])
 
 // String methods
+
 // 1) Extraction method
 let slicedStr = str.slice(2,7)
 console.log(slicedStr)
@@ -13,3 +14,9 @@ console.log(str)
 let slicedStr1 = str.slice(6)
 console.log(slicedStr1)
 console.log(str)
+
+let slicedStr2 = str.slice(6,-2)
+console.log(slicedStr2)
+console.log(str)
+
+// 2) Substring Methods
