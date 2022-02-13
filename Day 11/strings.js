9cd7303a92290a79363cc927be9df10ddf46076b
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-02-13 22:54:51 +0530

    InitialCommit

---
+++ Day 11/strings.js
@@ -0,0 +1,15 @@
+var str = "Hello guys"
+console.log(str)
+console.log(str[0])
+console.log(str[4])
+console.log(str[6])
+
+// String methods
+// 1) Extraction method
+let slicedStr = str.slice(2,7)
+console.log(slicedStr)
+console.log(str)
+
+let slicedStr1 = str.slice(6)
+console.log(slicedStr1)
+console.log(str)
