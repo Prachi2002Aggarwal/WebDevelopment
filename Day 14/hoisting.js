c8da957abb15196d8b2fc1c2a3ece8147a1a1bd7
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-02-23 20:13:23 +0530

    InitialCommit

---
+++ Day 14/hoisting.js
@@ -0,0 +1,45 @@
+/*
+var x = 10;
+function greet() {
+    console.log("hello guyss");
+}
+console.log(x);
+greet();*/
+
+/*var x = 10;
+console.log(x);
+greet();
+function greet() {
+    console.log("hello guyss");
+}*/
+
+/*console.log(x);
+greet();
+var x = 10;
+function greet() {
+    console.log("hello guyss");
+}*/
+
+/*
+console.log(x);
+greet();
+hello();
+var x = 10;
+function greet() {
+    console.log("hello guyss");
+}
+var hello = function() {
+    console.log("You guys");
+}*/
+
+console.log(x);
+greet();
+console.log(hello)
+hello();
+var x = 10;
+function greet() {
+    console.log("hello guyss");
+}
+var hello = function() {
+    console.log("You guys");
+}
