7b56eb8566320c8f472b34439b8c98cda184ebd9
Prachi2002Aggarwal <prachi2002aggarwal@gmail.com>
2022-03-08 19:54:18 +0530

    InitialCommit

--- Day17/closure.js
+++ Day17/closure.js
@@ -1,9 +1,35 @@
+/*
 function outer() {
     var a = 10;
     function inner() {
         console.log(a);
     }
     return inner;
+}
+var z = outer();
+console.log(z)
+z();*/
+
+/*
+function outer() {
+    var a = 7;
+    function inner() {
+        console.log(a);
+    }
+    a = 10;
+    return inner;
+}
+var z = outer();
+console.log(z)
+z();*/
+
+function outer() {
+    var a = 7;
+    function inner() {
+        console.log(a);
+    }
+    return inner;
+    a = 10;
 }
 var z = outer();
 console.log(z)
